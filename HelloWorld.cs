d77171c36c9680b48ef21f64f29e3f6bd6384977
Omer Tairi <tairiomer04@gmail.com>
2026-03-30 21:24:49 +0200

    first commit

---
+++ HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class HelloWorld
+{
+  public static void Main(string[] args)
+  {
+    int followers = 22; // numra tplot
+    double peshaOrizit = 28.5; // numra decimal;
+    string emriAkademis = "Edumind Academy (2026) 0.05241"; // tekst
+    bool majmOr = true; // 0 apo 1
+
+    
+  }
+}
