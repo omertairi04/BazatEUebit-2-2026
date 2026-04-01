958369c4d0fe9e80efc4ae8fed22d029f58815dc
Omer Tairi <tairiomer04@gmail.com>
2026-04-01 21:38:13 +0200

    1 Prill 2026

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,13 +1,52 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
 using System;
 
 public class HelloWorld
 {
-  public static void Main(string[] args)
-  {
-    int followers = 22; // numra tplot
-    double peshaOrizit = 28.5; // numra decimal;
-    string emriAkademis = "Edumind Academy (2026) 0.05241"; // tekst
-    bool majmOr = true; // 0 apo 1
-    Console.WriteLine("Hello World");
-  }
+    public static void Main(string[] args)
+    {
+        int followers = 22; // numra tplot
+        double peshaOrizit = 28.5; // numra decimal;
+        string emriAkademis = "Edumind Academy (2026) 0.05241"; // tekst
+        bool majmOr = true; // 0 apo 1
+
+        // rezultatin e 1 + 1
+        int mbledhja = 1 + 1; // 2
+        Console.WriteLine(mbledhja);
+        Console.WriteLine("Hello from the other side");
+        mbledhja = 5 + 5;
+        Console.WriteLine(mbledhja);
+    
+        double muki=2.5-1.5;
+        Console.WriteLine(muki);
+        
+        int shumzimi = 10 * 10;
+        Console.WriteLine(shumzimi);
+        
+        double pjestimi = 39.0 / 7;
+        Console.WriteLine(pjestimi);
+    
+        int rroga = 600;
+        int numriIPuntorve = 24;
+        
+        int rrogaMujore = rroga * numriIPuntorve;
+        Console.WriteLine(rrogaMujore);
+    
+        rroga = 1000;
+        numriIPuntorve =50;
+        
+         rrogaMujore = rroga * numriIPuntorve;
+         
+        Console.WriteLine("Mas ndryshimit");
+        Console.WriteLine(rrogaMujore);
+    
+        
+    
+        // 2000000
+        
+        
+        
+    }
 }
