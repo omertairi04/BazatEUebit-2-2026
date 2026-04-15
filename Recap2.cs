85c6e6c014de93613763c0b789148b10102b419a
Omer Tairi <tairiomer04@gmail.com>
2026-04-15 21:35:25 +0200

    15 Prill 2026

---
+++ Recap2.cs
@@ -0,0 +1,27 @@
+using BazatEUebit2;
+
+public class Recap2
+{
+  public static void Main(string[] args) {
+    
+    // Sintaksa per me kriju variabel:
+    // DataType emriVariables = vlerenEvariables;
+
+    // Data Types: (Llojet e variablave):
+    // int -> numra tplot (- pakufi deri ne + pakufi)
+    // double -> numra decimal (numra me prejse)
+    // string -> shkronja, tekst (duhet te jet ne thojza)
+    // bool -> vlera true ose false
+
+    string Edumind = "Edumind Academy";
+    int vitet = 21;
+    double kgMolla = 57.5;
+    string emriAkademis = Edumind;
+    bool bjeShi = false;
+
+    string vitetMas5Viteve = vitet + "5"; // 215 a 26
+    Console.WriteLine(vitetMas5Viteve);
+
+
+  }
+}
