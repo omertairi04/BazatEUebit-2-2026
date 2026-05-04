ea947fc814a583200f414f2562327701591f4b5a
Omer Tairi <tairiomer04@gmail.com>
2026-05-04 21:20:04 +0200

    4 Maj

---
+++ ForLoops.cs
@@ -0,0 +1,41 @@
+using System;
+
+public class ForLoops
+{
+  public static void Main(string[] args)
+  {
+    
+    int numri = 0;
+    
+    // for (vlera fillestare e ciklit; 
+    // kushti per me u ekzekutu kodi;
+    // per sa e rrismi ciklin
+    // ) {
+    // pjese e kodit qe egzekutohet xhdo cikel
+    // }
+
+    for (int zh=1; zh<=50;zh++)
+    {
+      if(zh % 2 == 0)
+      {
+        Console.WriteLine("Numri " + zh + " esht cift");
+      } 
+      else
+      {
+        Console.WriteLine("Numri " + zh + " esht tek");
+      }
+    }
+
+
+    for (int i=20; i>=1;i--) {
+          Console.WriteLine(i);
+      
+          
+      }
+
+      
+    Console.WriteLine(numri);
+
+
+  }
+}
