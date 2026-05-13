0d83f7e795d57ec3aff3ed37520dc08d5c6531c2
Omer Tairi <tairiomer04@gmail.com>
2026-05-13 21:34:42 +0200

    13 Maj

---
+++ Arrays2.cs
@@ -0,0 +1,48 @@
+using System;
+public class Arrays2
+{
+  public static void Main(string[] args)
+  {
+    
+    // string[] nxensit = new string[4];
+
+    // nxensit[0] = "Almir Aga";
+    // nxensit[1] = "Baci Hamza";
+    // nxensit[3] = "Rajan";
+
+    // for (int i=0;i<nxensit.Length; i++)
+    // {
+    //   Console.WriteLine(nxensit[i]);
+    // }
+
+    // string emri = "Omer";
+    // char[] emri = ['O','m','e','r'];
+    // emri[1] = '\0';
+
+    // for (int i=0;i<emri.Length;i++)
+    // {
+    //   Console.Write(emri[i]);
+    // }
+
+    int[] nums = [2,7,11,15];
+    int target = 26;
+
+    for (int i=0; i<nums.Length;i++)
+    {
+
+      for (int j=i+1; j<nums.Length; j++)
+      {
+        if (nums[i] + nums[j] == target)
+        {
+          Console.WriteLine(i + " " + j);
+        }
+        
+      }
+
+
+    }
+
+
+
+  }
+}
